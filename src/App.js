f252db77508d99e562d8070b0755271271e645a0
sayo-deca <sayoa@decagonhq.com>
2021-03-26 13:58:59 +0100

    updated package.json

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
     <div>
       <Router>
         <Switch>
-          <Route exact path='/open-sky' component={SignIn}/>
+          <Route exact path='/' component={SignIn} />
+          <Route path='/open-sky' component={SignIn}/>
           <Route path='/dashboard' component={Dashboard} />
         </Switch>
       </Router>
